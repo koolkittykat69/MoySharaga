7dc57857ab78fce529ff308777451b54bdaa8ed0
Alex <dudnickalex@gmail.com>
2020-03-15 22:53:42 +0200

    admin update

--- MoyaSharaga/MoyaSharaga/apps/panel/admin.py
+++ MoyaSharaga/MoyaSharaga/apps/panel/admin.py
@@ -4,17 +4,30 @@ from django.contrib import admin
 from .models import University, Faculty, Department, Specialty, Group, Student, Teacher, Subject, Mark
 
 class UniversityAdmin(admin.ModelAdmin):
-    list_display = ("university_short_title", "university_title",)
+    list_display = ("university_short_title", "university_title")
 
 class FacultyAdmin(admin.ModelAdmin):
-    list_display = ("university", "faculty_short_title","faculty_title",)
+    list_display = ("faculty_short_title", "faculty_title", "university")
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("department_short_title", "department_title", "faculty")
+
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ("specialty_title", "department")
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("group_title", "specialty")
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_surname", "student_name", "student_patronymic", "group")
+
 
 admin.site.register(University, UniversityAdmin)
 admin.site.register(Faculty, FacultyAdmin)
-admin.site.register(Department)
-admin.site.register(Specialty)
-admin.site.register(Group)
-admin.site.register(Student)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Specialty, SpecialtyAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Teacher)
 admin.site.register(Subject)
 admin.site.register(Mark)
