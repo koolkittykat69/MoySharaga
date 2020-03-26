254740b2d56a9f305872441c7d0d9481f6c0d81e
Alex <dudnickalex@gmail.com>
2020-03-26 12:56:15 +0200

    User Accounts v0.2.1

--- MoyaSharaga/MoyaSharaga/settings.py
+++ MoyaSharaga/MoyaSharaga/settings.py
@@ -132,3 +132,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
--- MoyaSharaga/MoyaSharaga/urls.py
+++ MoyaSharaga/MoyaSharaga/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from panel import views
 from users import views as user_views
@@ -21,7 +22,9 @@ from users import views as user_views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('panel/', include('panel.urls')),
-    path('register/', user_views.register, name="register"),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('grappelli', include('grappelli.urls')),
     path('admin/', admin.site.urls), # admin site
 ]
